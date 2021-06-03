3ef43beb175e4ae0c3c3e41cb1b9a1e8cd02d4c4
Rokas <rookas.rudzenskas@gmail.com>
2021-06-03 16:46:58 +0300

    Roaks

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 9000;
 /// api routes d
 // we just make the method, and one of them is downloading data, then url we go is www.something.com/ 👉 this is the base url and show the hello world
 // we set the status 200, which means okay 🎊
-app.get('/', (req, res) => res.status(200).send('hello rokas'));
+app.get('/', (req, res) => res.status(200).send('hello world'));
 // listener
 // we listen on the port in 👆 and console log it in here
 app.listen(port, () => console.log(`Listening on localhost:${port}`));
